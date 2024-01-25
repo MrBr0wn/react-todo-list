2dd624d8d3998480d2ae54b4ab2e41acdf37dd95
Mr. Brown <gitcherrypick.work@gmail.com>
2024-01-25 12:14:54 +0500

    build: add header template with styles

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -4,7 +4,24 @@ import TodoActions from '../TodoActions/TodoActions'
 import TodoRender from '../TodoRender/TodoRender'
 import './Todo.css'
 
+const initialFormData = {
+  isEdit: false,
+  todoName: '',
+  todoNote: '',
+  isFinished: false,
+  id: '',
+  index: null
+}
+
 function Todo() {
+  const [tab, setTab] = useState(0)
+  const [isOpen, setOpen] = useState(false)
+  const [isOpenDisplayTodo, setOpenDisplayTodo] = useState(false)
+  const [todo, setTodos] = useState([])
+  const [formData, setFormData] = useState(initialFormData)
+
+  const handleOpenDialog = () => setIsOpen((prevState) => !prevState)
+
   return (
     <div className='todo-wrapper'>
       <TodoHeader />
--- src/components/TodoHeader/TodoHeader.jsx
+++ src/components/TodoHeader/TodoHeader.jsx
@@ -1,9 +1,63 @@
 import './TodoHeader.css'
+import moment from 'moment'
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+
+const whiteColor = '#fff'
+const styles = {
+  wrapper: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '95%'
+  },
+  finished: {
+    fontSize: '46px',
+    color: whiteColor
+  },
+  total: {
+    display: 'flex',
+    flexDirection: 'column',
+    color: whiteColor 
+  },
+  weekDay: {
+    color: whiteColor,
+    fontSize: '28px'
+  },
+  date: {
+    color: whiteColor,
+    fontSize: '28px',
+    marginLeft: 10
+  }
+}
 
 function TodoHeader() {
+  const weekDay = moment().format('dddd')
+  const date = moment().date()
+
   return (
-    <div>
-  
+    <div className='todo-header'>
+      <div style={styles.wrapper}>
+        <div className="todos-count">
+          <span style={styles.finished}>2</span>
+
+          <div style={styles.total}>
+            <span>Tasks</span>
+            <span>/ 10</span>
+          </div>
+        </div>
+
+        <div>
+          <span style={styles.weekDay}>{weekDay}</span>
+          <span style={styles.date}>{date}</span>
+        </div>
+      </div>
+
+      <div>
+        <div className="add-todo">
+          <AddCircleIcon color='primary' />
+          <span className='icon-background'></span>
+        </div>
+      </div>
     </div>
   )
 }
